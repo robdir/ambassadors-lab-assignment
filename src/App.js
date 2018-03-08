a7283e0fa2a9a91d22df64aaa99d83132d91d869
Machiel-B <a.m.banen@gmail.com>
2018-03-08 14:06:55 +0100

    feature(frontend): add dev-test briefing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './the_ambassadors_logo.svg';
+import mockup from './mock-up.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +9,39 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Lab</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="App-intro">
+          <section>
+            <h1>Briefing</h1>
+            <p>
+              We want to build small photo stream prototype in React.
+              It needs to load photos from a rest endpoint
+              and display them in a feed.
+            </p>
+            <p>
+              Since we do not have a backend (yet) we will
+              use fake data from <a href="https://jsonplaceholder.typicode.com/photos?_start=0&_end=10">JsonPlaceholder</a>
+            </p>
+
+            <ul>
+              <li>Each photo should display with its title overlayed</li>
+              <li>Each photo should have a (fake) "like" button</li>
+              <li>Initially the app should only load 3 photos</li>
+              <li>Users should be able to load more photos</li>
+              <li>Feel free to add your own requirements</li>
+            </ul>
+            <p>
+              Good luck!
+            </p>
+          </section>
+          <figure>
+            <figcaption>
+              <small>Mock up of the app</small>
+            </figcaption>
+            <img src={mockup} alt="a wireframe mockup showing a picture feed" />
+          </figure>
+        </main>
       </div>
     );
   }
